ad7f601e54c6245cdcb00ee25613295ba983bab9
Iurii-Antoniuk <iurii.antoniuk@gmail.com>
2020-04-29 01:16:06 +0200

    moreUi

--- Checkpoint_WCircus/UserControlFeedback.xaml.cs
+++ Checkpoint_WCircus/UserControlFeedback.xaml.cs
@@ -22,5 +22,10 @@ namespace Checkpoint_WCircus
         {
             InitializeComponent();
         }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- Database_Api/Feedback.cs
+++ Database_Api/Feedback.cs
@@ -10,8 +10,6 @@ namespace Database_Api
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public string City { get; set; }
         public string Comment { get; set; }
-
     }
 }
